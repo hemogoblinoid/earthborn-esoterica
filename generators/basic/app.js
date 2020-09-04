3ca515affd4abb461879f13e728bcfe04382b394
Hemo <38544696+hemogoblinoid@users.noreply.github.com>
2020-09-04 11:33:30 -0500

    add favicon

---
+++ generators/basic/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const encounters = [{"description": "Mundane Animals"},{ "description":"People" },{ "description":"Intelligent Animal"},{"description":"Chimera"},{"description":"Something weird"},{"description":"Something really weird"}];
+
+function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("div");
+      div.innerHTML = '<strong>Description:</strong> ' + data[i].description;
+      mainContainer.appendChild(div);
+    }
+  }
+
+appendData(encounters);
