6048db09ffdeb6a0c54566d1006f5fa6e0af8773
Hemo <38544696+hemogoblinoid@users.noreply.github.com>
2020-09-04 13:33:53 -0500

    reformat files

--- generators/basic/app.js
+++ generators/basic/app.js
@@ -1,14 +1,21 @@
-'use strict'
+"use strict";
 
-const encounters = [{"description": "Mundane Animals"},{ "description":"People" },{ "description":"Intelligent Animal"},{"description":"Chimera"},{"description":"Something weird"},{"description":"Something really weird"}];
+const encounters = [
+  { description: "Mundane Animals" },
+  { description: "People" },
+  { description: "Intelligent Animal" },
+  { description: "Chimera" },
+  { description: "Something weird" },
+  { description: "Something really weird" }
+];
 
 function appendData(data) {
-    var mainContainer = document.getElementById("myData");
-    for (var i = 0; i < data.length; i++) {
-      var div = document.createElement("div");
-      div.innerHTML = '<strong>Description:</strong> ' + data[i].description;
-      mainContainer.appendChild(div);
-    }
+  var mainContainer = document.getElementById("myData");
+  for (var i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = "<strong>Description:</strong> " + data[i].description;
+    mainContainer.appendChild(div);
   }
+}
 
 appendData(encounters);
